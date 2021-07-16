74d6b8c0830b40e587413761adf0029d9d8b6620
Steve <steve.paduchowski@gmail.com>
2021-07-16 10:32:33 -0400

    Adding Directory Lightbox Global

---
+++ Directory (Global)/directoryPopupGlobal.js
@@ -0,0 +1,21 @@
+function directoryLightboxGlobal() {
+
+        $('.faculty-staff-list .biobutton').on('click', function() {
+
+            //set email address
+            var getHref = $(this).closest('div').find('.email');
+            var getHref = getHref.children('a');
+            var newMailto = 'mailto:' + getHref.attr('data-username') + "@" + getHref.attr("data-domain");
+            getHref.attr("href", newMailto);
+
+            //setup and load lightbox
+            var directoryDetails = $(this).parents("li").html();
+            var directoryDetails = '<div class="directory-popup">' + directoryDetails + '</div>';
+
+            SimpleLightbox.open({
+                content: directoryDetails,
+                elementClass: 'slbContentEl'
+            });
+
+        });
+}
