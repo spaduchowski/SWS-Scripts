30e5ba808f7f34dddf248f101aabd27dbbd47e20
Steve <steve.paduchowski@gmail.com>
2023-01-11 14:53:27 -0500

    adding Auto Lightbox

---
+++ Auto Lightbox/event/How to Add to Page.md
@@ -0,0 +1,39 @@
+##Auto Event Lightbox Instructions
+
+###General Notes
+- The popup is set to display after user is on page for 1.5 seconds. This can be adjusted
+- The popup will only display during the Start Date/Time and End Date/Time you have input.
+- If a user clicks the Stop Showing This Popup link, they will no longer see this popup.
+- If you make changes to the title, dates or text, the popup will again display for any user who clicked the Stop Showing This Popup link.
+ 
+
+###How to Add Lightbox Popup to Page
+ 
+1. Add event widget to page.
+2. Select category #AutoLighbox.
+3. Click pencil icon to edit lightbox content.
+4. Select date and times to display lightbox. (Start date and end date are required.)
+5. Enter text into details box.
+6. To Edit or create a call-to-action button, see steps below.
+7. Click Style button in left-hand column of page.
+8. Scroll to event widget and select.
+9. Click dropdown menu in left-hand column of page.
+10. Select SPL: Auto Lightbox.
+11. Save page.
+ 
+#####To edit existing call-to-action button
+1. Select text with mouse.
+2. Click three dot icon to expand menu.
+3. Click link icon.
+4. Edit URL and Text to display fields.
+5. Click Save.
+ 
+#####To create call-to-action button
+1. Type button text in details box.
+2. Select this text with mouse.
+3. Click three dot icon to expand menu.
+4. Click link icon.
+5. Edit URL and Text to display fields.
+6. Click Save.
+7. Click B bold icon.
+ 
---
+++ Auto Lightbox/list/autolistlightbox.js
@@ -0,0 +1,82 @@
+function createAutoListLightbox(autoLightboxStyle, autoLightboxClass, autoLightboxDelay) {
+
+    //concat style
+    var theBlockStyle = '.style-' + autoLightboxStyle;
+
+    //check to see if required elements exist on page before proceeding and exit this function if they don't
+    var checkListsEl = document.querySelector(theBlockStyle + " .lists");
+    if (checkListsEl === null) {
+       return;
+    }   
+
+    /**
+     * Get Popup Content
+     */
+    var theContent = document.querySelector(theBlockStyle).outerHTML;
+    //create stop showing link
+    var theCloseButton = '<div class="stop-showing"><a href="#" id="closeLink">Stop Showing This Popup</a></div>';
+
+
+    /**
+     * Create Dynamic "Cookie" Using
+     * List Title & Character Count
+     */
+    //get list title
+    var listTitle = document.querySelector(theBlockStyle + ' .lists .h4-style');
+
+    //get character count
+    checkListsEl = checkListsEl.innerHTML.length;
+
+    //create cookie using list title and character count
+    var autoCookie = listTitle.textContent + checkListsEl;
+    console.log(autoCookie)
+
+    
+    /**
+     * Create Lightbox Instance,
+     * Attach Stop Showing Link
+     * and Set Content
+     */
+    //create popup instance
+    var autoLightbox = new SimpleLightbox({
+        elementClass: autoLightboxClass,
+        closeBtnClass: 'close'
+    });
+
+    //attach stop showing link
+    theContent = theContent.innerHTML = "<div>" + theContent + theCloseButton + "</div>"
+    //set content
+    autoLightbox.setContent(theContent);
+
+
+
+    /**
+     * Set Dynamic "Cookie" When
+     * Stop Showing When Link is Clicked
+     */
+
+    //checking browser for our cookie
+    const setCookieValue = localStorage.getItem(autoCookie);
+
+    //dynamic click function to detect if clicked, set cookie and close lightbox
+    document.addEventListener("click", function(e) {
+        const target = e.target.closest("#closeLink"); // Or any other selector.
+
+        if (target) {
+            localStorage.setItem(autoCookie, true);
+            autoLightbox.close();
+        }
+    });
+
+
+    //load popup between start date&time and end date, if cookie has been set, don't load
+    if (setCookieValue != 'true') {
+        setTimeout(function() {
+            autoLightbox.show();
+        }, autoLightboxDelay)
+        console.log("Auto List Lightbox is currently displaying.")
+    } else {
+        console.log("User clicked stop showing link.");
+    }
+
+} //end auto list lightbox function
